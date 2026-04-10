f55d9622cbed348a6cca843cb55c6a13408bb6ec
gustavobee <gustavobee0@gmail.com>
2026-04-10 16:07:46 -0300

    add first basics endpoints

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
 const app = express();
+const book = [];
+app.post('/books', (req, res) => {
+    console.log(req);
+    res.send('Ok post teste');
+});
 app.get('/books', (req, res) => {
     res.send('Hello World');
 });
--- index.ts
+++ index.ts
@@ -1,8 +1,21 @@
 import express from 'express';
 const app = express();
+app.use(express.json());
+
+const books: string[] = [];
+
+app.post('/books', (req, res) => {
+    books.push(req.body);
+    res.send('Ok post');
+})
 
 app.get('/books', (req, res) => {
-    res.send('Hello World');
+    res.json(books);
+})
+
+app.delete('/books', (req, res) => {
+    books.pop();
+    res.send('Ok delete');
 })
 
 app.listen(3000);
