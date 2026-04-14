d4b5143124ea8ca2d959ffe52df5d184611bf340
Gustavo Bee Campos Rocha <127756249+gustavobee@users.noreply.github.com>
2026-04-14 17:03:23 +0000

    add try-catch

--- index.ts
+++ index.ts
@@ -10,44 +10,75 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+// GET
 app.get("/books", async (req, res) => {
-  const books = await selectBooks();
-  res.status(200).json(books);
+  try {
+    const books = await selectBooks();
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar livros" });
+  }
 });
 
+// GET ONE
 app.get("/books/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
+  try {
+    const id = parseInt(req.params.id);
 
-  if (isNaN(id)) {
-    return res
-      .status(400)
-      .json({ error: "O ID precisa ser um número válido." });
+    if (isNaN(id)) {
+      return res
+        .status(400)
+        .json({ error: "O ID precisa ser um número válido." });
+    }
+
+    const book = await selectBook(id);
+
+    if (!book) {
+      return res.status(404).json({ error: "Livro não encontrado." });
+    }
+
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar livro" });
   }
-
-  const book = await selectBook(id);
-
-  if (!book) {
-    return res.status(404).json({ error: "Livro não encontrado." });
-  }
-
-  res.status(200).json(book);
 });
 
+// POST
 app.post("/books", async (req, res) => {
-  await insertBook(req.body);
-  res.sendStatus(201);
+  try {
+    const newBook = await insertBook(req.body);
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao inserir livro" });
+  }
 });
 
+// PUT
 app.put("/books/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-  await updateBook(id, req.body);
-  res.sendStatus(200);
+  try {
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) return res.status(400).json({ error: "ID inválido" });
+
+    await updateBook(id, req.body);
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao atualizar livro" });
+  }
 });
 
+// DELETE
 app.delete("/books/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-  await deleteBook(id);
-  res.sendStatus(204);
+  try {
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) return res.status(400).json({ error: "ID inválido" });
+
+    await deleteBook(id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao deletar livro" });
+  }
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Servidor ligado");
+});
